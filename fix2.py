7394f7dcfb8524eafbd39aca58424f9e370d3ef7
erikmartinelly <89890233+erikmartinelly@users.noreply.github.com>
2026-08-26 00:25:57 -0400

    chore: remove temp scripts

--- fix2.py
+++
@@ -1,8 +0,0 @@
-import sys
-with open('js/admin.js', 'r', encoding='utf-8') as f:
-    c = f.read()
-start = c.find('async function descargarEstadisticasGeneralesCSV()', c.find('async function descargarEstadisticasGeneralesCSV()') + 10)
-end = c.find('async function switchAdminTabAvisos()', start)
-c = c[:start] + c[end:]
-with open('js/admin.js', 'w', encoding='utf-8') as f:
-    f.write(c)
